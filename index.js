907803b1bfd4a17735f605251885624b65c6ba19
Eric Delmer Millen <ericdelmermillen@gmail.com>
2023-08-09 12:31:53 -0400

    first commit on develop

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-
 const { getRandomDuration, getRandomLikesCount, getRandomViewsCount} = require("./utils"); 
 // const {fromRoutes} = require("./routes/videos"); 
 
