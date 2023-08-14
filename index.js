b983eb9757683ada84a682d6cc791cbf2784bb1a
Eric <ericdelmermillen@gmail.com>
2023-08-13 23:56:48 -0400

    refactored whole server. can not get static images to connect to the proper paths

--- index.js
+++ index.js
@@ -1,113 +1,15 @@
-const cors = require('cors');
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-const router = express.Router();
-const routes = require("./routes/videos").router; 
-
-
+const cors = require('cors');
 const app = express();
- 
-const corsOptions = {
-  origin: 'http://localhost:3000'
-}
+const videosRouter = require('./routes/videos');
 
 app.use(express.json());
-app.use(cors(corsOptions));
 
-require('dotenv').config();
+app.use(express.static('./public'));
 
-// Allow access to assets folder
-app.use(express.static('./assets'));
+app.use(cors({origin: 'http://localhost:3001'}));
 
 
-// allows access to built in json parsing method
-app.use(express.json());
+app.use('/videos', videosRouter);
 
-
-// path to videos.json file
-const videosFilePath = './data/videos.json';
-
-
-
-
-
-// Routes:
-
-// // get videos request for /
-// get videos request for /videos
-app.use('/videos', routes);
-
-
-// // get video details request for /videos:id
-// app.use('/videos/:id', routes);
-
-
-
-
-
-
-
-
-// request handler for post videos; needs:
-  // id
-  // comments
-  // description
-  // duration
-  // likes
-  // timestamp
-  // title
-  // views
-  
-// channel
-// image, 
-// video
-
-// app.post('/videos', (req, res) => {
-//   const newVideo = req.body;
-
-//   if (!newVideo.title || !newVideo.description) {
-//     return res.status(400).json({ error: 'All videos require a title and a description' });
-//   }
-
-//   newVideo.id = uuidv4();
-//   newVideo.channel = 'some channel';
-//   newVideo.duration = getRandomDuration();
-//   newVideo.likes = getRandomLikesCount();
-//   newVideo.views = getRandomViewsCount();
-//   newVideo.comments = [];
-//   newVideo.timestamp = new Date().getTime();
-
-//   // Read existing videos data
-//   fs.readFile(videosFilePath, (err, data) => {
-//     if (err) {
-//       console.error('Error reading videos.json:', err);
-//       return res.status(500).json({ error: 'Internal server error' });
-//     }
-
-//     try {
-//       const videos = JSON.parse(data);
-//       videos.unshift(newVideo);
-
-//       // Write updated videos data back to the file
-//       fs.writeFile(videosFilePath, JSON.stringify(videos, null, 2), err => {
-//         if (err) {
-//           console.error('Error writing videos.json:', err);
-//           return res.status(500).json({ error: 'Internal server error' });
-//         }
-
-//         res.json({ message: 'Video added successfully', video: newVideo });
-//       });
-//     } catch (parseError) {
-//       console.error('Error parsing JSON:', parseError);
-//       res.status(500).json({ error: 'Error parsing JSON' });
-//     }
-//   });
-// });
-
-
-// access env variables at process.env.VARIABLE_NAME
-const port = process.env.PORT || 8088;
-
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.listen(8080, () => console.log(`🚀 Listening on 8080`));
--- routes/videos.js
+++ routes/videos.js
@@ -1,35 +1,41 @@
-const express = require('express');
-const router = express.Router();
 const fs = require('fs');
-const videosFilePath = './data/videos.json';
-const { getRandomDuration, getRandomLikesCount, getRandomViewsCount} = require("../utils"); 
+const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const router = express.Router();
 
+const videosFilePath = './data/videos.json';
 
-// get videos request
-router.route('/')
-  .get((req, res) => {
-  fs.readFile(videosFilePath, (err, data) => {
-    try {
+const { getRandomDuration, getRandomLikesCount, getRandomViewsCount} = require("../utils"); 
 
-      const videosEssentailData = JSON.parse(data).map(video => {
-        const { comments, description, duration, likes, timestamp, video: videoFile, views, ...essentials } = video;
-        return essentials;
-      });
-      
-      res.json(videosEssentailData);
-      
-    } catch (error) {
-      console.error(error);
-    }
-  })
-  // post videos
-}).post((req, res) => {
+// Helper function to get video data
+const getVideos = () => JSON.parse(fs.readFileSync(videosFilePath));
+
+// Helper function to save video data
+
+const validateCreateVideo = (req, res, next) => {
   const newVideo = req.body;
-
   if (!newVideo.title || !newVideo.description) {
     return res.status(400).json({ error: 'All videos require a title and a description' });
+  } else {
+    next();
   }
+  return newVideo;
+}
+
+// get videos request
+router
+  .route('/')
+  .get((req, res) => {
+    const videosData = getVideos();  
+    
+    const videosEssentailData = videosData.map(video => {
+      const { comments, description, duration, likes, timestamp, video: videoFile, views, ...essentials } = video;
+      return essentials;
+      })
+      res.status(200).json(videosEssentailData);
+  })
+.post((req, res) => {
+  const newVideo = req.body;
 
   newVideo.id = uuidv4();
   newVideo.channel = 'some channel';
@@ -38,103 +44,71 @@ router.route('/')
   newVideo.views = getRandomViewsCount();
   newVideo.comments = [];
   newVideo.timestamp = new Date().getTime();
-  newVideo.image = "https://i.imgur.com/l2Xfgpl.jpg"
+  newVideo.image = "images/l2Xfgpl.jpg"
 
   // Read existing videos data
   fs.readFile(videosFilePath, (err, data) => {
+
+  const videos = JSON.parse(data);
+  videos.unshift(newVideo);
+
+  // Write updated videos data back to the file
+  fs.writeFile(videosFilePath, JSON.stringify(videos, null, 2), err => {
     if (err) {
-      console.error('Error reading videos.json:', err);
+      console.error('Error writing videos.json:', err);
       return res.status(500).json({ error: 'Internal server error' });
-    }
-
-    try {
-      const videos = JSON.parse(data);
-      videos.unshift(newVideo);
-
-      // Write updated videos data back to the file
-      fs.writeFile(videosFilePath, JSON.stringify(videos, null, 2), err => {
-        if (err) {
-          console.error('Error writing videos.json:', err);
-          return res.status(500).json({ error: 'Internal server error' });
-        }
-
-        res.json({ message: 'Video added successfully', video: newVideo });
-      });
-    } catch (parseError) {
-      console.error('Error parsing JSON:', parseError);
-      res.status(500).json({ error: 'Error parsing JSON' });
-    }
+    }});
   });
+    res.json({ message: 'Video added successfully', video: newVideo });
 });
-
 
 
 router.route('/:id')
 // get video details
   .get((req, res) => {
-  
-  const requestedId = req.params.id; 
+    
+  const id = req.params.id; 
   
   fs.readFile(videosFilePath, (err, data) => {
-
-      const requestedVideo = JSON.parse(data).find(video => video.id === requestedId);
+    const requestedVideo = JSON.parse(data).find(video => video.id === id);
       
     if (!requestedVideo) {
       return res.status(404).json({ error: 'Video not found' });
     }
     res.json(requestedVideo);
   })
-  // post comments on a video
 })
+
 router.post('/:id/comments', (req, res) => {
-  const requestedId = req.params.id; 
-  const newComment = req.body;
-
-  
+  // post comments on a video
   fs.readFile(videosFilePath, (err, data) => {
-       
-    try {
-      const videos = JSON.parse(data);
-
-      const requestedVideo = videos.find(video => video.id === requestedId);
-
-      if (!requestedVideo) {
-        return res.status(404).json({ error: 'Video not found' });
-      }
-
-      if (!requestedVideo.comments) {
-        requestedVideo.comments = [];
-      }
-      requestedVideo.comments.push(newComment);
-
-      // Write the updated data back to the JSON file
-      fs.writeFile(videosFilePath, JSON.stringify(videos, null, 2), err => {
-        if (err) {
-          console.error('Error writing videos.json:', err);
-          return res.status(500).json({ error: 'Internal server error' });
-        }
-
-        res.json({ message: 'Comment added successfully', video: requestedVideo });
-      });
-
-    } catch (parseError) {
-      console.error('Error parsing JSON:', parseError);
-      res.status(500).json({ error: 'Error parsing JSON' });
+    
+    const videos = JSON.parse(data);
+    const requestedVideo = videos.find(video => video.id === req.body.id);
+    
+    if (!requestedVideo) {
+      return res.status(404).json({ error: 'Video not found' });
     }
+    
+    if (!requestedVideo.comments) {
+      requestedVideo.comments = [];
+    }
+    const newComment = req.body;
+    newComment.id = uuidv4();
+    newComment.name = "Yo mama...";
+    newComment.likes = getRandomLikesCount();
+    newComment.timestamp = new Date().getTime();
+    requestedVideo.comments.unshift(newComment);
+
+    // Write the updated data back to the JSON file
+    fs.writeFile(videosFilePath, JSON.stringify(videos, null, 2), err => {
+      if (err) {
+        console.error('Error writing videos.json:', err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+      res.json({ message: 'Comment added successfully', video: requestedVideo });
+    });
   });
 });
 
-
-
-
-
-
-
-
-
-module.exports = {
-  router
-//   getRandomLikesCount,
-//   getRandomViewsCount,
-//   getRandomDuration
-}
+module.exports = router;
